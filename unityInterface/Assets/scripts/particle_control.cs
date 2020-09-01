b6d03fce38fc2e14be30635f23a42738b48dd9b7
mb2882 <42128793+mb2882@users.noreply.github.com>
2020-08-31 21:51:57 -0400

    Update particle_control.cs

--- unityInterface/Assets/scripts/particle_control.cs
+++ unityInterface/Assets/scripts/particle_control.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class particle_control : MonoBehaviour {
 
-	public int channel = 0;
-	public int audio_component = 3;
+	public GameObject spectrumAsset;
+	private SimpleSpectrum spectrumAnalyzer;
+	public ParticleSystem ps;
 	public Material asset_mat;
-	public float radiusMag = 1000f;
+	public float radiusMag = 100f;
 	public float emissionMag = 100f;
 	public float startSizeMag = 10f;
-	public float speedMultiplier = 50f;
+	public float lifetimeMag = 20f;
+	public float rotationMag = 10f;
+	public float hueVariationMag = 2f;
 
 	private float H, S, V, start_H, final_H;
 	private Color startColor;
 	private Color tmpColor;
 
-
-	public ParticleSystem ps;
 	private ParticleSystem.ShapeModule pShape;
 	private ParticleSystem.MainModule pMain;
 	private ParticleSystem.VelocityOverLifetimeModule vMain;
@@ -26,6 +27,7 @@ public class particle_control : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		spectrumAnalyzer = spectrumAsset.GetComponent<SimpleSpectrum>();
 		//ps = GetComponent<ParticleSystem>();
 		pShape = ps.shape;
 		pMain = ps.main;
@@ -35,8 +37,10 @@ public class particle_control : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//pShape.radius = SimpleSpectrum.spectrum[0] * radiusMag;
-		pShape.angle = Mathf.Abs(OSC_channels.OSCch_data[channel,audio_component]) * 10f;
+		pShape.radius = 0.3f + SimpleSpectrum.spectrum[0] * radiusMag;
+		pShape.angle = 10f;
+		pMain.startLifetime = 1f - Mathf.Clamp01(SimpleSpectrum.spectrum[10] * lifetimeMag);
+		pMain.startRotation = SimpleSpectrum.spectrum[36] * rotationMag;
 		
 
 		//move object up and down
@@ -47,13 +51,14 @@ public class particle_control : MonoBehaviour {
 		Color.RGBToHSV(startColor, out H, out S, out V);
 
 		//color adjustments in hue
-		start_H = Mathf.Clamp01(SimpleSpectrum.spectrum[0] * 10000f);
+		start_H = (SimpleSpectrum.spectrum[1] * 10000f * hueVariationMag) % 1.0f;
+		Debug.Log(start_H);
 
 		//particle emission and size adjustments
-		float tmp_emission = SimpleSpectrum.spectrum[0] * 10000f * emissionMag;
+		float tmp_emission = SimpleSpectrum.spectrum[1] * 10000f * emissionMag;
 		ps.emissionRate = tmp_emission;
-		float tmpSize = SimpleSpectrum.spectrum[0] * 1000f * startSizeMag;
-		//pMain.startSize = new ParticleSystem.MinMaxCurve(tmpSize + 2f, tmpSize + 5f);
+		float tmpSize = SimpleSpectrum.spectrum[1] * 1000f * startSizeMag;
+		pMain.startSize = new ParticleSystem.MinMaxCurve(tmpSize + 0.1f, tmpSize + 1f);
 
 		//float tmp_speed = OSC_channels.OSCch_data[channel,2] * speedMultiplier;
 		//pMain.startSpeed = new ParticleSystem.MinMaxCurve(tmp_speed+12f, tmp_speed+15f);
